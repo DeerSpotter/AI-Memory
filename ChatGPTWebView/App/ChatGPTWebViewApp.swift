4e3ad94bfbd066b7baad9022013ead2c5f8c06bb
DeerSpotter <m.teleguz1@gmail.com>
2026-07-04 10:13:24 -0400

    Check latest release on app startup

--- ChatGPTWebView/App/ChatGPTWebViewApp.swift
+++ ChatGPTWebView/App/ChatGPTWebViewApp.swift
@@ -3,13 +3,18 @@ import SwiftUI
 @main
 struct ChatGPTWebViewApp: App {
     @StateObject private var appModel = AppModel()
+    @StateObject private var updateChecker = AppUpdateChecker()
 
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(appModel)
+                .environmentObject(updateChecker)
                 .task {
                     await appModel.restoreSession()
+                }
+                .task {
+                    await updateChecker.checkForUpdate()
                 }
                 .onOpenURL { url in
                     appModel.handleOpenURL(url)
